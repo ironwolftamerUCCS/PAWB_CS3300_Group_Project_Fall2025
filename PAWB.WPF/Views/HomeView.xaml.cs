2d8062af2113db359b8e44830b014db0ca8fd609
trinityjfalcon <trinity.falcon1@gmail.com>
2025-11-28 14:57:30 -0700

    fixed?????????

--- PAWB.WPF/Views/HomeView.xaml.cs
+++ PAWB.WPF/Views/HomeView.xaml.cs
@@ -1,4 +1,9 @@
-﻿using PAWB.WPF.Models;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using PAWB.Domain.Model;
+using PAWB.EntityFramework;
+using PAWB.EntityFramework.Services;
+using PAWB.WPF.Models;
+using PAWB.WPF.Views;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -43,7 +48,7 @@ namespace PAWB.WPF.Views
             TriGrey = new Cursor(Application.GetResourceStream(new Uri("Cursors/greywhitecursorpaw.cur", UriKind.Relative)).Stream);
 
             this.Loaded += async (_, __) => await LoadEntrysAsync();
-        
+
         }
 
         private async Task LoadEntrysAsync()
@@ -57,141 +62,133 @@ namespace PAWB.WPF.Views
 
                 // If service returned null, leave Items empty so UI displays nothing
                 if (entrys == null)
-                {
-                    await Application.Current.Dispatcher.InvokeAsync(() => Items.Clear());
-                    return;
-                }
-                new InfoItem { Title = "Account 1", Description = "Description1"},
-                new InfoItem { Title = "Account 2", Description = "Description2"},
-                new InfoItem { Title = "Account 3", Description = "Description3"},
-                new InfoItem { Title = "Account 4", Description = "Description4"},
-                new InfoItem { Title = "Account 5", Description = "Description5"},
-                new InfoItem { Title = "Account 6", Description = "Description6"},
-            };
-            DataContext = this;
-
-                // Update collection on UI thread
-                await Application.Current.Dispatcher.InvokeAsync(() =>
-                {
-                    Items.Clear();
-                    foreach (var u in entrys)
-                    {
-                        Items.Add(new InfoItem
-                        {
-                            Title = u.Username,
-                            Description = u.Email
-                        });
-                    }
-                });
+            {
+                await Application.Current.Dispatcher.InvokeAsync(() => Items.Clear());
+                return;
             }
+
+            // Update collection on UI thread
+            await Application.Current.Dispatcher.InvokeAsync(() =>
+            {
+                Items.Clear();
+                foreach (var u in entrys)
+                {
+                    Items.Add(new InfoItem
+                    {
+                        Title = u.Username,
+                        Description = u.Email
+                    });
+                }
+            });
+        }
             catch (Exception ex)
             {
                 // Show a simple error - adjust logging as needed
                 MessageBox.Show($"Failed to load entries: {ex.Message}", "Load Error", MessageBoxButton.OK, MessageBoxImage.Error);
             }
-        }
+}
 
-        private void DetailButton_Click(object sender, RoutedEventArgs e)
-        {
-            var item = (sender as FrameworkElement)?.DataContext as InfoItem;
-            if (item != null)
-            {
-                var win = new DetailWindow(item);
-                win.ShowDialog();
-            }
+private void DetailButton_Click(object sender, RoutedEventArgs e)
+{
+    var item = (sender as FrameworkElement)?.DataContext as InfoItem;
+    if (item != null)
+    {
+        var win = new DetailWindow(item);
+        win.ShowDialog();
+    }
 
-        }
+}
 
-        // Custom theme
-        private void OnToggleButtonChecked(object sender, RoutedEventArgs e)
-        {
-            _isDefaultTheme = !_isDefaultTheme;
-            string newThemePath = _isDefaultTheme ? "Themes/Default.xaml" : "Themes/ScaryPawble.xaml";
-            var newTheme = (ResourceDictionary)Application.LoadComponent(new Uri(newThemePath, UriKind.Relative));
-            Application.Current.Resources.MergedDictionaries.RemoveAt(0);
-            Application.Current.Resources.MergedDictionaries.Add(newTheme);
-        }
+// Custom theme
+private void OnToggleButtonChecked(object sender, RoutedEventArgs e)
+{
+    _isDefaultTheme = !_isDefaultTheme;
+    string newThemePath = _isDefaultTheme ? "Themes/Default.xaml" : "Themes/ScaryPawble.xaml";
+    var newTheme = (ResourceDictionary)Application.LoadComponent(new Uri(newThemePath, UriKind.Relative));
+    Application.Current.Resources.MergedDictionaries.RemoveAt(0);
+    Application.Current.Resources.MergedDictionaries.Add(newTheme);
+}
 
-        // Custom Paw Cursors
-        private void btnGrey_Click(object sender, RoutedEventArgs e)
-        {
-            if (sender == btnGrey)
-            {
-                this.Cursor = Grey;
-            }
-            else if (sender == btnBrown)
-            {
-                this.Cursor = Brown;
-            }
-            else if (sender == btnWhiteGrey)
-            {
-                this.Cursor = WhiteGrey;
-            }
-            else if (sender == btnTriGrey)
-            {
-                this.Cursor = TriGrey;
-            }
-        }
+// Custom Paw Cursors
+private void btnGrey_Click(object sender, RoutedEventArgs e)
+{
+    if (sender == btnGrey)
+    {
+        this.Cursor = Grey;
+    }
+    else if (sender == btnBrown)
+    {
+        this.Cursor = Brown;
+    }
+    else if (sender == btnWhiteGrey)
+    {
+        this.Cursor = WhiteGrey;
+    }
+    else if (sender == btnTriGrey)
+    {
+        this.Cursor = TriGrey;
+    }
+}
 
-        private void btnBrown_Click(object sender, RoutedEventArgs e)
-        {
-            if (sender == btnGrey)
-            {
-                this.Cursor = Grey;
-            }
-            else if (sender == btnBrown)
-            {
-                this.Cursor = Brown;
-            }
-            else if (sender == btnWhiteGrey)
-            {
-                this.Cursor = WhiteGrey;
-            }
-            else if (sender == btnTriGrey)
-            {
-                this.Cursor = TriGrey;
-            }
-        }
+private void btnBrown_Click(object sender, RoutedEventArgs e)
+{
+    if (sender == btnGrey)
+    {
+        this.Cursor = Grey;
+    }
+    else if (sender == btnBrown)
+    {
+        this.Cursor = Brown;
+    }
+    else if (sender == btnWhiteGrey)
+    {
+        this.Cursor = WhiteGrey;
+    }
+    else if (sender == btnTriGrey)
+    {
+        this.Cursor = TriGrey;
+    }
+}
 
-        private void btnWhiteGrey_Click(object sender, RoutedEventArgs e)
-        {
-            if (sender == btnGrey)
-            {
-                this.Cursor = Grey;
-            }
-            else if (sender == btnBrown)
-            {
-                this.Cursor = Brown;
-            }
-            else if (sender == btnWhiteGrey)
-            {
-                this.Cursor = WhiteGrey;
-            }
-            else if (sender == btnTriGrey)
-            {
-                this.Cursor = TriGrey;
-            }
-        }
+private void btnWhiteGrey_Click(object sender, RoutedEventArgs e)
+{
+    if (sender == btnGrey)
+    {
+        this.Cursor = Grey;
+    }
+    else if (sender == btnBrown)
+    {
+        this.Cursor = Brown;
+    }
+    else if (sender == btnWhiteGrey)
+    {
+        this.Cursor = WhiteGrey;
+    }
+    else if (sender == btnTriGrey)
+    {
+        this.Cursor = TriGrey;
+    }
+}
 
-        private void btnTriGrey_Click(object sender, RoutedEventArgs e)
-        {
-            if (sender == btnGrey)
-            {
-                this.Cursor = Grey;
-            }
-            else if (sender == btnBrown)
-            {
-                this.Cursor = Brown;
-            }
-            else if (sender == btnWhiteGrey)
-            {
-                this.Cursor = WhiteGrey;
-            }
-            else if (sender == btnTriGrey)
-            {
-                this.Cursor = TriGrey;
-            }
-        }
+private void btnTriGrey_Click(object sender, RoutedEventArgs e)
+{
+    if (sender == btnGrey)
+    {
+        this.Cursor = Grey;
+    }
+    else if (sender == btnBrown)
+    {
+        this.Cursor = Brown;
+    }
+    else if (sender == btnWhiteGrey)
+    {
+        this.Cursor = WhiteGrey;
+    }
+    else if (sender == btnTriGrey)
+    {
+        this.Cursor = TriGrey;
+    }
+}
 
     }
 }
