46760935c535b2102e580196439ac1bc3648d981
ironwolftamerUCCS <bstampe2@uccs.edu>
2025-11-29 15:50:18 -0700

    Trying to get DB working...

--- PAWB.Domain.Tests/obj/Debug/net9.0/PAWB.Domain.Tests.AssemblyInfo.cs
+++ PAWB.Domain.Tests/obj/Debug/net9.0/PAWB.Domain.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PAWB.Domain.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b90ea920f99fb3f0ea3e80eab87f3d75aa4b68f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c4e8c19152c39ab9f955d24c12991a3cf7b3c0b")]
 [assembly: System.Reflection.AssemblyProductAttribute("PAWB.Domain.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PAWB.Domain.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
