c057d9f66eaca3d033860826003bb5da123a959e
Evan Futey <ejf.colorado@gmail.com>
2025-11-29 16:36:49 -0700

    Add login to home navigation

--- PAWB.Domain/obj/Debug/net9.0/PAWB.Domain.AssemblyInfo.cs
+++ PAWB.Domain/obj/Debug/net9.0/PAWB.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PAWB.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b90ea920f99fb3f0ea3e80eab87f3d75aa4b68f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27049e54590728a774c9d1c07bcc52ae498be522")]
 [assembly: System.Reflection.AssemblyProductAttribute("PAWB.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PAWB.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
